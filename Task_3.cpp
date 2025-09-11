12fc190db48d20512638602069ae73cf154e84e2
Defordes <wadimser258@gmail.com>
2025-09-11 14:42:11 +0300

    Version plus

--- Task_3.cpp
+++ Task_3.cpp
@@ -1,17 +1,39 @@
 #include <iostream>
 #include <cmath>
+#include <iomanip>
 int main() { 
 setlocale(LC_ALL, "RU");
 double start, end, step; 
 std::cout << "Введите начальное значение x: "; 
-std::cin >> start; // Задаем исходное значение переменной 
+std::cin >> start; 
 std::cout << "Введите конечное значение x: "; 
-std::cin >> end; // Задаем конечно значение переменной 
+std::cin >> end; 
 std::cout << "Введите шаг: "; 
-std::cin >> step; // Задаем шаг, с которым с которым будет изменяться аргумент 
-for (double x = start; x <= end; x += step) { 
-double y = sin(x); // функция y = sin(x)   
-std::cout << x << " | " << y << std::endl; // Выводим значения функции для каждого аргумента (x | y) 
-} 
-return 0; 
-} 
+std::cin >> step;
+if (step == 0) {
+    std::cout << "Ошибка: шаг не может быть равен 0!" << std::endl;
+    return 1;
+}
+if ((start < end && step < 0) || (start > end && step > 0)) {
+    std::cout << "Ошибка: знак шага не соответсвует направлению интервала!" << std::endl;
+    return 1;
+}
+std::cout << std::fixed << std::setprecision(6);
+if (start < end) {
+    for (double x = start; x <= end; x += step) {
+        double y = sin(x);
+        std::cout << "x =" << x << " | Y =" << y << std::endl;
+    }
+} else {
+    for (double x = start; x >= end; x += step) {
+    double y = sin(x);
+        std::cout << "x = " << x << " y = " << y << std::endl;
+        }
+    
+}
+return 0;
+}
+
+    
+
+
